cf107d319d622f3effba9db2fd3364049d464f66
Marius Schär <schaer.marius@gmail.com>
2015-05-21 09:24:54 +0200

    caller done
    caller done, now do sort algorithms.

--- sorting/Form1.vb
+++ sorting/Form1.vb
@@ -9,18 +9,6 @@
         timer.Stop()
         Dim timespan As TimeSpan = timer.Elapsed
         lblExecTime.Text = "Execution Time[ms]: " & timespan.TotalMilliseconds.ToString()
-        'Dim timer As Stopwatch = Stopwatch.StartNew()
-        'Select Case lstAlgo.SelectedIndex
-        '    Case 0 : rtfOutput.Text = sortBubble(rtfInput.Text)
-        '    Case 1 : rtfOutput.Text = sortRipple(rtfInput.Text)
-        '    Case 2 : rtfOutput.Text = sortIntern(rtfInput.Text)
-        '    Case 3 : rtfOutput.Text = sortQuick(rtfInput.Text, 1, Len(rtfInput.Text) - 1)
-        '    Case 4 : rtfOutput.Text = sortInsertion(rtfInput.Text)
-        '    Case Else : MessageBox.Show("Please Select a sorting algorithm")
-        'End Select
-        'timer.Stop()
-        'Dim timespan As TimeSpan = timer.Elapsed
-        'lblExecTime.Text = "Execution Time[ms]: " & timespan.TotalMilliseconds.ToString
     End Sub
 
     Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
--- sorting/modUtil.vb
+++ sorting/modUtil.vb
@@ -6,9 +6,9 @@ Imports System.Linq
 #End Region
 Module modUtil
 #Region "swap"
+    'funktion zum austauschen zweier elemente in einem StringArray
     Function swap(strIn As String(), pos1 As Integer, pos2 As Integer) As String()
         Dim temp As String
-
         temp = strIn(pos1)
         strIn(pos1) = strIn(pos2)
         strIn(pos2) = temp
@@ -16,55 +16,107 @@ Module modUtil
     End Function
 #End Region
 #Region "Umlaute"
+    'falls ein non-ascii char vorhanden ist, werden die zeichen entfernt und als ascii char zurückgegeben.
     Function uml(ByVal strIn As String) As String
+        'sehe ob chkUmlaute gewählt ist.
         If Form1.chkUmlaute.Checked Then
+            'Deklariere und Initisalisiere StringBuilder
             Dim sb As New StringBuilder
+            'loope durch inputString Char für Char
             For j As Integer = 0 To strIn.Length - 1 Step 1
+                'Splitte den inputchar in zeichen und ascii-char
                 Dim ns As String = strIn(j).ToString.Normalize(NormalizationForm.FormD)
+                'loope durch diese zeichen
                 For i As Integer = 0 To ns.Length - 1 Step 1
+                    'Hole .NET unicodekategorie des jeweiligen zeichens
                     Dim uc As UnicodeCategory = CharUnicodeInfo.GetUnicodeCategory(ns.Chars(i))
+                    'Falls die Unicodekategorie NICHT NonSpacingMark
+                    'http://www.fileformat.info/info/unicode/category/Mn/list.htm
+                    '^^^ liste der nonspacingmarks ^^^
                     If uc <> UnicodeCategory.NonSpacingMark Then
+                        'füge den char am StringBuilder an.
                         sb.Append(ns.Chars(i))
                     End If
                 Next
             Next
+            'Normalisiere wieder zurück vom unicode spec zum vb.net spec
             Return sb.ToString.Normalize(NormalizationForm.FormC).ToString()
         Else
+            'falls umlaute nicht ausgewähl ist, gebe den input wieder zurück
             Return strIn
         End If
     End Function
 #End Region
 #Region "caller"
-    'calls sort functions with parameters
+    'präpariert inputstring zum sortieren, und ruft den entsprechenden sortieralgorithmus auf.
     Sub callSort(ByVal strSort As String, ByVal alg As Integer, ByVal words As Boolean)
-        Dim tmps, tmpp As String()
+        'deklariere temp variabeln
+        Dim tmpstr, tmppunc As String()
+        Dim tmplist As New List(Of String)
+        'strippe sonderzeichen vom inputstring und speichere diese in <strPunct> ab.
         Dim strPunct As String = stripPunctuation(strSort)
+
+        'Wenn Wortsortierung
         If words Then
-            tmps = strSort.Split(New Char() {" "}, StringSplitOptions.RemoveEmptyEntries)
+            'Teile inputstring in array auf, geteilt bei leerzeichen
+            tmpstr = strSort.Split(New Char() {" "}, StringSplitOptions.RemoveEmptyEntries)
         Else
-            Dim tmplist As New List(Of String)
+            'Konvertiere String zu String() für jeden Char
             For Each c As Char In strSort
                 If Not c = " " Then
                     tmplist.Add(c.ToString())
                 End If
             Next
-            tmps = tmplist.ToArray()
+            tmpstr = tmplist.ToArray()
+            tmplist.Clear()
         End If
-        tmpp = strPunct.Split(New Char() {""}, StringSplitOptions.RemoveEmptyEntries)
-        Dim val(tmps.Length + tmpp.Length - 1) As String
-        Array.Copy(tmpp, val, tmpp.Length)
-        Array.Copy(tmps, 0, val, tmpp.Length, tmps.Length)
+
+        'Konvertiere String zu String() für jeden Char, sonderzeichen
+        For Each c As Char In strPunct
+            If Not c = " " Then
+                tmplist.Add(c.ToString())
+            End If
+        Next
+        tmppunc = tmplist.ToArray()
+        tmplist.Clear()
+
+        'füge Sonderzeichen zum beginn der "normalen" Strings hinzu
+        Dim val(tmpstr.Length + tmppunc.Length - 1) As String
+        Array.Copy(tmppunc, val, tmppunc.Length)
+        Array.Copy(tmpstr, 0, val, tmppunc.Length, tmpstr.Length)
+        'output ins textfeld
         strSort = String.Join(vbCrLf, val)
         Form1.rtfOutput.Text = strSort
-        MessageBox.Show(strPunct)
+
+        'rufe sortieralgorithmen auf
+        Select Case alg
+            Case 0
+                'sortBubble()
+            Case 1
+                'sortRipple()
+            Case 2
+                'sortIntern()
+            Case 3
+                'sortQuick()
+            Case 4
+                'sortInsertion()
+            Case Else
+                MessageBox.Show("Bitte wählen Sie einen Sortieralgorithmus!", "Achtung!", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Select
     End Sub
 #End Region
 #Region "strip punctuation"
+    'entfernt sonderzeichen durch verwendung von <IsPunctuation> und gibt diese als string zurück.
     Function stripPunctuation(ByRef strIn As String) As String
+        'Deklariere & initialisiere StringBuilder
         Dim sb As New StringBuilder
+        'loop durch inputstring
         For i As Integer = 0 To strIn.Length - 1 Step 1
+            'sehe ob Char ein Sonderzeichen ist
             If Char.IsPunctuation(strIn(i)) Then
+                'füge sonderzeichen zu Stringbuilder hinzu
                 sb.Append(strIn(i))
+                'Ersetze sonderzeichen durch " " in inputstring
                 Mid(strIn, i + 1, 1) = " "
             End If
         Next
